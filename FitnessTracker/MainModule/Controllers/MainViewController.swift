35c4b80ed0fb677717486279a7ca48419850e87b
Anastasiya Harris <ufadesigner@gmail.com>
2022-10-08 23:51:50 +0500

    project created

---
+++ FitnessTracker/MainModule/Controllers/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FitnessTracker
+//
+//  Created by Админ on 05.10.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    private let userPhotoImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
